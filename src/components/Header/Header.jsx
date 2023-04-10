35057c07b69370266787b611e9170625c71a9021
nhanVoThanhdp05 <nhan.vo_dp05@devplus.edu.vn>
2023-04-10 17:25:40 +0700

    make-initial-setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import { Typography } from 'antd'
+import React from 'react'
+
+function Header() {
+  return (
+    <Typography.Text>Header</Typography.Text>
+  )
+}
+
+export default Header
---
+++ src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import { Typography } from 'antd'
+import React from 'react'
+import Header from '../components/Header/Header'
+
+function MainPage() {
+  return (
+    <Header />
+  )
+}
+
+export default MainPage
