5e1b32ca56787d1dbcbb298528a049b9be4ee864
nhanVoThanhdp05 <nhan.vo_dp05@devplus.edu.vn>
2023-04-11 14:41:45 +0700

    feat: dpm-setup-routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import MainPage from "./pages/MainPage";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Redirect,
+  Navigate,
+} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route
+            exact
+            path="/"
+            element={<Navigate to="/dashboard" replace />}
+          />
+          <Route
+            path="dashboard/*"
+            element={
+              <Suspense fallback={<>Loading...</>}>
+                <Dashboard />
+              </Suspense>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
---
+++ src/pages/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>Oops... Page not found</div>
+  )
+}
