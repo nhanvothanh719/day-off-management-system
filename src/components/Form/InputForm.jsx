c9e4f4c6be90b193b2e4d801d19c400fe4218399
nhanVoThanhdp05 <nhan.vo_dp05@devplus.edu.vn>
2023-04-16 16:39:17 +0700

    feat: dpm-calculate-date-off-amount

--- src/components/Form/InputForm.jsx
+++ src/components/Form/InputForm.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InputForm.scss";
-import { Button, Select, Form, DatePicker, Radio, InputNumber, Space } from "antd";
+import {
+  Button,
+  Select,
+  Form,
+  DatePicker,
+  Radio,
+  InputNumber,
+  Space,
+} from "antd";
 import TextArea from "antd/lib/input/TextArea";
 import dayjs from "dayjs";
-import customParseFormat from 'dayjs/plugin/customParseFormat';
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
 dayjs.extend(customParseFormat);
 
 function InputForm() {
   const [form] = Form.useForm();
-  const nameValue = Form.useWatch('name', form);
-  
+
+  const [dayOffAmount, setDayOffAmount] = useState(0);
+  const [dayOffSession, setDayOffSession] = useState('morning');
+
   const dateFormat = "YYYY/MM/DD";
 
   const initialValues = {
@@ -19,12 +29,26 @@ function InputForm() {
   };
 
   const onFinish = (values) => {
-    console.log(values);
     form.resetFields();
   };
 
+  const onDateRangeChange = (range) => {
+    const day_off_amount = (range[1]._d - range[0]._d)/3600/24/1000 + 1;
+    dayOffSession === "all_day" ? setDayOffAmount(day_off_amount) : setDayOffAmount(day_off_amount/2);
+  }
+
+  const onSessionChange = (option) => {
+    const previous_session = dayOffSession;
+    const day_off_amount = dayOffAmount;
+    setDayOffSession(option);
+    if(previous_session === "all_day" && option !== "all_day") {
+      setDayOffAmount(day_off_amount/2);
+    } else if(previous_session !== "all_day" && option === "all_day") {
+      setDayOffAmount(day_off_amount*2);
+    }
+  }
+
   const onChange = (value) => {
-    console.log("changed", value);
   };
 
   return (
@@ -34,54 +58,55 @@ function InputForm() {
       initialValues={initialValues}
       className="custom-form"
       layout="vertical"
-    >
+      >
       <Form.Item
         name="day_off_type"
         label="Types of day off"
-        rules={[{ required: true }]}
+        // rules={[{ required: true }]}
       >
-        <Radio.Group size='large'>
-        <Space direction="vertical">
-          <Radio value="off">Off</Radio>
-          <Radio value="wfh">Work from home</Radio>
+        <Radio.Group size="large">
+          <Space direction="vertical">
+            <Radio value="off">Off</Radio>
+            <Radio value="wfh">Work from home</Radio>
           </Space>
         </Radio.Group>
       </Form.Item>
-      
-    <Form.Item
-        name="day_off_length"
+
+      <Form.Item
+        name="day_off_range"
         label="From - To"
         rules={[{ required: true }]}
       >
-        <DatePicker.RangePicker
-      format={dateFormat}
-    />
+        <DatePicker.RangePicker format={dateFormat} onChange={onDateRangeChange} />
       </Form.Item>
-      <Form.Item name="session" 
-      label="Session" 
-      rules={[{ required: true }]}>
-        <Select onChange={onChange}>
+
+      <Form.Item
+        name="session"
+        label="Session"
+        // rules={[{ required: true }]}
+      >
+        <Select onChange={onSessionChange}>
           <Select.Option value="morning">Morning</Select.Option>
           <Select.Option value="afternoon">Afternoon</Select.Option>
           <Select.Option value="all_day">All day</Select.Option>
         </Select>
       </Form.Item>
+
       <Form.Item
-        name="day_off_quantity"
         label="Quantity"
-        rules={[{ required: true }]}
       >
-        <InputNumber min={1} max={7} defaultValue={0.5} onChange={onChange} />
+        <InputNumber disabled value={dayOffAmount} />
       </Form.Item>
-      <Form.Item name="reason" 
-      label="Reason" 
-      rules={[{ required: true }]}>
+
+      <Form.Item
+        name="reason"
+        label="Reason"
+        // rules={[{ required: true }]}
+      >
         <TextArea rows={4} showCount maxLength={100} />
       </Form.Item>
       <Form.Item>
-        <Button htmlType="submit">
-          Submit
-        </Button>
+        <Button htmlType="submit">Submit</Button>
       </Form.Item>
     </Form>
   );
