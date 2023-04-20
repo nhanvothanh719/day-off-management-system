835c511edd9b688cad2ce5467714494de32d37f5
HuuTrung <trung.doan_dp05@devplus.edu.vn>
2023-04-20 14:33:39 +0700

    feat:dpm-create-form-member-details

--- src/components/Manager/MemberDetails.jsx
+++ src/components/Manager/MemberDetails.jsx
@@ -1,7 +1,117 @@
 import React from "react";
-
-const MemberDetails = () => {
-  return <div>MemberDetails</div>;
+import { Button, Form, Input } from "antd";
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
 };
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+const MemberDetails = () => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="Information "
+      onFinish={onFinish}
+      style={{
+        maxWidth: 600,
+      }}
+      scrollToFirstError
+    >
+      <h2> Basic Information </h2>
+      <Form.Item
+        name="First Name"
+        label="First Name"
+        rules={[
+          {
+            required: true,
+            message: "Please input your name!",
+            whitespace: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="Last Name"
+        label="Last Name"
+        rules={[
+          {
+            required: true,
+            message: "Please input your name!",
+            whitespace: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: "email",
+            message: "The input is not valid E-mail!",
+          },
+          {
+            required: true,
+            message: "Please input your E-mail!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
 
+      <Form.Item
+        name="id" 
+        label="Slack Id"
+        rules={[
+          {
+            required: true,
+            message: "Please input your slack id number!",
+          },
+        ]}
+      >
+        <Input
+          style={{
+            width: "100%",
+          }}
+        />
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="Send">
+          Send
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 export default MemberDetails;
