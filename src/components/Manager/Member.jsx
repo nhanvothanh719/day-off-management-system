2b42c1de7402e00e4683c42eaa13634a3bd91b97
HuuTrung <trung.doan_dp05@devplus.edu.vn>
2023-04-21 14:34:30 +0700

    feat:dpm-create-table-display-member

--- src/components/Manager/Member.jsx
+++ src/components/Manager/Member.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-
-const Member = () => {
-  return <div>member</div>;
-};
-
+import { Table } from "antd";
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+  },
+];
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    email: "text1@gmail.com",
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    email: "text2@gmail.com",
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    email: "text3@gmail.com",
+  },
+];
+const Member = () => <Table columns={columns} dataSource={data} bordered />;
 export default Member;
--- src/pages/Manager/MemberPage.js
+++ src/pages/Manager/MemberPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Member from '../../components/Manager/member';
 
-const MemberPage = () => {
+function MemberPage(){
   return (
     <Member/>
   )
 }
 
-export default MemberPage
+export default MemberPage;
