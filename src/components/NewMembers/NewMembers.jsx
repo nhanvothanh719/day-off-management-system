15512a0cf4f6e84487aeb4928763ba6acde7d4c2
DatDevPlus <dat.phung_dp05@devplus.edu.vn>
2023-04-23 19:14:37 +0700

    feat: dpm-create-new-user

--- src/components/NewMembers/NewMembers.jsx
+++ src/components/NewMembers/NewMembers.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "./NewMembers.scss";
 import MyCard from "../Card/Card";
 import axiosClient from "../../utils/clientAxios";
@@ -7,12 +7,14 @@ import { Button, Form, Input, Radio, Select, Space, message } from "antd";
 const Member = () => {
   const [form] = Form.useForm();
   const [messageApi, contextHolder] = message.useMessage();
+  const [permissions, setPermissions] = useState({});
+  const [roles, setRoles] = useState({});
 
   const onFinish = (values) => {
     const { username, email, role, password, permission } = values;
     const new_user = {
-      role: role,
-      permission: permission,
+      role_id: role,
+      permission_id: permission,
       username: username,
       email: email,
       password: password,
@@ -22,7 +24,7 @@ const Member = () => {
 
   const storeUser = (new_user) => {
     axiosClient
-      .post("/", new_user)
+      .post("/users", new_user)
       .then(() => {
         form.resetFields();
         messageApi.open({
@@ -32,6 +34,33 @@ const Member = () => {
       })
       .catch((error) => console.log(error));
   };
+
+  useEffect(() => {
+    axiosClient
+      .get("/permissions")
+      .then((res) => {
+        setPermissions(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axiosClient
+      .get("/auth/getAllRole")
+      .then((res) => {
+        setRoles(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+  }
 
   const LogOffForm = (
     <Form
@@ -43,31 +72,34 @@ const Member = () => {
       <Form.Item name="role" label="Role of user" rules={[{ required: true }]}>
         <Radio.Group size="large">
           <Space direction="vertical">
-            <Radio value="admin">Admin</Radio>
-            <Radio value="manager">Manager</Radio>
-            <Radio value="staff">Staff</Radio>
+            {roles?.role?.map((role) => (
+              <Radio value={role._id} onChange={handleChange}>
+                {role.role_name}
+              </Radio>
+            ))}
           </Space>
         </Radio.Group>
       </Form.Item>
 
       <Form.Item
-        name="permimssion"
+        name="permission"
         label="Permission"
         rules={[{ required: true }]}
       >
         <Select mode="multiple" style={{ width: "100%" }}>
-          <Select.Option value="dashboard">Dashboard</Select.Option>
-          <Select.Option value="request">Request</Select.Option>
-          <Select.Option value="day_off">Day off</Select.Option>
-          <Select.Option value="member">Member</Select.Option>
-          <Select.Option value="group">Group</Select.Option>
-          <Select.Option value="notification">Notification</Select.Option>
-          <Select.Option value="sync">Sync</Select.Option>
-          <Select.Option value="workspace">Workspace</Select.Option>
+          {permissions?.permissions?.map((permission) => (
+            <Select.Option value={permission._id} onChange={handleChange}>
+              {permission.permission_detail}
+            </Select.Option>
+          ))}
         </Select>
       </Form.Item>
-
-      <Form.Item name="username" label="Username" rules={[{ required: true }]}>
+      <Form.Item
+        name="username"
+        label="Username"
+        onChange={handleChange}
+        rules={[{ required: true }]}
+      >
         <Input placeholder="Username" />
       </Form.Item>
       <Form.Item name="email" label="Email" rules={[{ required: true }]}>
