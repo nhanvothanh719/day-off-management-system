fb8c2cb3f142d6c71bbd9690dcae5d33153412aa
Vo Thanh Nhan <126637281+nhanVoThanhdp05@users.noreply.github.com>
2023-04-28 09:20:44 +0700

    DPM-Fix-Group (#42)

--- src/components/GroupDetail/GroupDetail.jsx
+++ src/components/GroupDetail/GroupDetail.jsx
@@ -10,7 +10,7 @@ import {
   Select,
   Modal,
   Tag,
-  message
+  message,
 } from "antd";
 import "./GroupDetail.scss";
 import { useParams } from "react-router-dom";
@@ -28,11 +28,11 @@ const GroupDetail = () => {
 
   const [staffs, setStaffs] = useState([]);
   const [openModal, setOpenModal] = useState(false);
-  const [selectedMemberId, setSelectedMemberId] = useState('');
+  const [selectedMemberId, setSelectedMemberId] = useState("");
 
   const findNameById = (id) => {
     const staff = staffs.find((master) => master._id === id);
-    return staff ? staff.name : '';
+    return staff ? staff.name : "";
   };
 
   const staffTagRender = (props) => {
@@ -57,76 +57,73 @@ const GroupDetail = () => {
   };
 
   const addNewMember = () => {
-    selectedMemberId &&
-      axiosClient
-        .put("/groups/add-group-staff", {
-          staff_id: selectedMemberId,
-          group_id: id,
-        })
-        .then((res) => {
-          setGroupStaffs(res.data.staffs);
-          messageApi.open({
-            type: "success",
-            content: 'Add new member successfully',
-        });
-        })
-        .catch((error) => {
-            messageApi.open({
-                type: "error",
-                content: 'This staff is already a group member!',
-            });
-        })
-        setOpenModal(false);
+    if (
+      groupStaffs.some(staff => staff._id === selectedMemberId) ||
+      groupMasters.some(master => master._id === selectedMemberId)
+    ) {
+      messageApi.open({
+        type: "error",
+        content: "This staff is already a group member!",
+      });
+    } else {
+      messageApi.open({
+        type: "success",
+        content: "Add new member successfully",
+      });
+      setGroupStaffs([
+        ...groupStaffs,
+        { _id: selectedMemberId, name: findNameById(selectedMemberId) },
+      ]);
+    }
+    setOpenModal(false);
   };
 
   const addNewMaster = (staffId) => {
-    setGroupMasters([...groupMasters, {_id: staffId, name: findNameById(staffId)}]);
+    setGroupMasters([
+      ...groupMasters,
+      { _id: staffId, name: findNameById(staffId) },
+    ]);
     setGroupStaffs(groupStaffs.filter((staff) => staff._id !== staffId));
-  }
+  };
 
   const removeMaster = (masterId) => {
-    setGroupStaffs([...groupStaffs, {_id: masterId, name: findNameById(masterId)}]);
+    setGroupStaffs([
+      ...groupStaffs,
+      { _id: masterId, name: findNameById(masterId) },
+    ]);
     setGroupMasters(groupMasters.filter((master) => master._id !== masterId));
-  }
+  };
 
-  const removeMember = (value) => {
-    axiosClient
-      .put("/groups/remove-group-staff", { staff_id: value, group_id: id })
-      .then((res) => {
-        setGroupStaffs(res.data.staffs);
-        messageApi.open({
-            type: "success",
-            content: 'Remove member successfully',
-        });
-    });
+  const removeMember = (staffId) => {
+    setGroupStaffs(groupStaffs.filter((staff) => staff._id !== staffId));
   };
 
   const handleGroupNameChange = (event) => {
     setGroupName(event.target.value);
-  }
+  };
 
   const updateGroup = (event) => {
     const updatedGroup = {
-        name: groupName,
-        staffs_id: groupStaffs.map((staff) => staff._id),
-        masters_id: groupMasters.map((master) => master._id),
-    }
-    if(groupName.length <= 0) {
+      name: groupName,
+      staffs_id: groupStaffs.map((staff) => staff._id),
+      masters_id: groupMasters.map((master) => master._id),
+    };
+    if (groupName.length <= 0) {
       messageApi.open({
         type: "error",
-        content: 'Name of group is required!',
-    });
+        content: "Name of group is required!",
+      });
     } else {
       axiosClient.put("/groups/" + id, updatedGroup).then((res) => {
-        if(res.data.success) {
-            messageApi.open({
-                type: "success",
-                content: 'Update group details successfully',
-            });
+        if (res.data.success) {
+          messageApi.open({
+            type: "success",
+            content: "Update group details successfully",
+          });
         }
-    })
+      });
     }
-  }
+  };
 
   useEffect(() => {
     async function getGroupInfo() {
@@ -162,7 +159,7 @@ const GroupDetail = () => {
         </Button>
       }
     >
-        {contextHolder}
+      {contextHolder}
       <Modal
         title="Add new member"
         open={openModal}
@@ -185,7 +182,6 @@ const GroupDetail = () => {
       </Modal>
 
       <Space direction="vertical" size={20} style={{ width: "100%" }}>
-
         <Row gutter={[0, 12]}>
           <Col xs={24} sm={24} md={5} lg={5} xl={5} xxl={5}>
             <Typography.Text style={{ color: "black", fontWeight: "500" }}>
@@ -213,11 +209,8 @@ const GroupDetail = () => {
               onSelect={addNewMaster}
             >
               {groupStaffs.map((staff) => (
-                <Select.Option
-                  key={staff._id}
-                  value={staff._id}
-                >
-                    {staff.name}
+                <Select.Option key={staff._id} value={staff._id}>
+                  {staff.name}
                 </Select.Option>
               ))}
             </Select>
@@ -243,7 +236,9 @@ const GroupDetail = () => {
         </Row>
 
         <Space className="update-btn__area">
-          <Button className="update-group__button" onClick={updateGroup}>Update</Button>
+          <Button className="update-group__button" onClick={updateGroup}>
+            Update
+          </Button>
         </Space>
       </Space>
     </Card>
